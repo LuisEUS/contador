d4d1fbbc25f008843ef1235397d87608eddefcb9
Luis Eduardo Uscanga Serrano <isc18350503@gmail.com>
2022-04-05 10:09:26 -0500

    Se añadio el input de datos numericos al contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,83 @@
 import { useState } from "react";
 
-
 function App() {
 
   const [cuenta, setCuenta] = useState(0);
+  const [paso, setPaso] = useState(0);
 
   const handleClic = () => {
-    setCuenta(cuenta+1);
+    setCuenta(cuenta+paso);
   }
   const handleMenosClic = () => {
-      setCuenta(cuenta-1);
+      setCuenta(cuenta-paso);
   }
     const handleResetClic = () => {
       setCuenta(cuenta - cuenta);
-  };
-
+      setPaso(1);
+  }
+    const handleInputChange = (event) => {
+      if (isNaN(event.target.value)){
+        return;
+      }
+      setPaso(Number(event.target.value));
+    }
 
   return (
-    <div className="App">
-      
+    <div className="App">      
         
         <h3>Contador</h3>
      
         <hr />
         <h2 className="text-center">{cuenta}</h2>
         <hr />
-        <div style={{display: "flex", justifyContent: "flex-end", marginRight: "5px",}}>
-          
-          <button type="button" className="btn btn-primary" onClick={() => {
-            handleClic()
-            }}>+1</button>
 
-            <button type="button" className="btn btn-primary mx-2" onClick={() => {
-            handleMenosClic()
-            }}>-1</button>
-          
-          <button type="button" className="btn btn-primary mx-2" onClick={() => {
-            handleResetClic()
-            }}>Reinicio</button>
-          
-        </div>
+        <div 
+        style={{
+        display: "flex", 
+        justifyContent: "flex-end", 
+        marginRight: "15px"
         
-      
+        }}>
+          
+        <label>
+          Paso
+          <input 
+          id="paso"
+          name="paso"
+          type="text" 
+          value={paso}
+          onChange={handleInputChange} 
+          style={{
+            marginLeft: "15px", 
+            width: "60px"
+            }}/>
+        </label>
+
+          <button 
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClic}
+          style={{marginLeft:"15px"}}
+        >+1
+        </button>
+
+          <button 
+          type="button" 
+          className="btn btn-primary mx-2" 
+          onClick={handleResetClic}
+          style={{marginLeft:"15px"}}
+        >Reinicio
+        </button>
+
+            <button 
+            type="button" 
+            className="btn btn-primary mx-2" 
+            onClick={handleMenosClic}
+            style={{marginLeft:"15px"}}
+            >-1
+            </button>
+   
+        </div>
         
     </div>
   );
