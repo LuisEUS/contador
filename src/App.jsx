645016ef8c138afb63ba113b4cc8bdaae8de89fd
Luis Eduardo Uscanga Serrano <isc18350503@gmail.com>
2022-04-05 09:19:27 -0500

    Se añadieron 2 botones: Resta y Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
-//TODO:
+
+
 function App() {
+
   const [cuenta, setCuenta] = useState(0);
 
   const handleClic = () => {
-    setCuenta(cuenta + 1);
+    setCuenta(cuenta+1);
+  }
+  const handleMenosClic = () => {
+      setCuenta(cuenta-1);
+  }
+    const handleResetClic = () => {
+      setCuenta(cuenta - cuenta);
   };
+
 
   return (
     <div className="App">
-      <h3>Contador</h3>
-      <hr />
-      <h2 className="text-center">{cuenta}</h2>
-      <hr />
-      <div style={{
-      display: "flex", 
-      justifyContent: "flex-end", 
-      marginRight:"5px",
-      }}
-      >
-            <button type="button" 
-        className="btn btn-primary" 
-        onClick={handleClic}>+1
-        </button>
-      </div>
+      
+        
+        <h3>Contador</h3>
+     
+        <hr />
+        <h2 className="text-center">{cuenta}</h2>
+        <hr />
+        <div style={{display: "flex", justifyContent: "flex-end", marginRight: "5px",}}>
+          
+          <button type="button" className="btn btn-primary" onClick={() => {
+            handleClic()
+            }}>+1</button>
+
+            <button type="button" className="btn btn-primary mx-2" onClick={() => {
+            handleMenosClic()
+            }}>-1</button>
+          
+          <button type="button" className="btn btn-primary mx-2" onClick={() => {
+            handleResetClic()
+            }}>Reinicio</button>
+          
+        </div>
+        
+      
+        
     </div>
   );
 }
